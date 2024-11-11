3a5be55837f3f6574fc10828d94f39802659ed47
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-11-11 13:35:20 +0530

    Update districtProcedure.sql

--- plSQL/districtProcedure.sql
+++ plSQL/districtProcedure.sql
@@ -1,9 +1,18 @@
 CREATE or REPLACE PROCEDURE set_status AS
     DISTRICT varchar(20);
+	CURSOR cur IS
+        SELECT S.District FROM Students S JOIN
+        Student_rank R on S.roll_no=R.roll_no
+        WHERE R.rank<100;
+	--cur_record Students%ROWTYPE;--this is used to fetch a row but we currently need to fetch the column in this program
 	BEGIN
-		SELECT S.DISTRICT into district FROM STUDENTS S JOIN 
-		Student_rank R ON S.roll_no=R.roll_no where R.RANK<100;
-		DBMS_OUTPUT.PUT_LINE('District: '||district);
+        open cur;
+		LOOP
+			FETCH cur INTO DISTRICT;
+			EXIT when cur%NOTFOUND;
+			DBMS_OUTPUT.PUT_LINE('District: '||DISTRICT);
+		END LOOP;
+		CLOSE cur;
 	END;
 /
 BEGIN 
