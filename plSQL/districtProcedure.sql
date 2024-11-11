0e462f4934bb24064f95669cdfa5be88e6a566f7
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-11-11 12:43:45 +0530

    Create districtProcedure.sql

---
+++ plSQL/districtProcedure.sql
@@ -0,0 +1,11 @@
+CREATE or REPLACE PROCEDURE set_status AS
+    DISTRICT varchar(20);
+	BEGIN
+		SELECT S.DISTRICT into district FROM STUDENTS S JOIN 
+		Student_rank R ON S.roll_no=R.roll_no where R.RANK=1;
+		DBMS_OUTPUT.PUT_LINE('District: '||district);
+	END;
+/
+BEGIN 
+	set_status;
+END;
