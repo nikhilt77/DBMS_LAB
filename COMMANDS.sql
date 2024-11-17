b36322f8077eb6b96765b9d1d6bf3b72fac6828c
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-11-17 13:39:34 +0530

    Update COMMANDS.sql

--- COMMANDS.sql
+++ COMMANDS.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE STOREDB;
 USE STOREDB;
 SHOW TABLES;
+```
+ select e.name,d.dname from employee e 
+   join department d on d.dno=e.dno 
+   join project p on d.dno=p.dno where d.dno=5 
+   and not exists(select p.pno from project p where p.dno=d.dno 
+   and not exists(select w.pno from works_on w where w.pno=p.pno and e.ssn=w.ssn));
