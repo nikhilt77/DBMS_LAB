545ba05894e8ad2666579910711d6655868e1fc7
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-11-17 21:01:30 +0530

    Update stockTrigger.sql

--- stockTrigger.sql
+++ stockTrigger.sql
@@ -1,23 +1,31 @@
-
-create or replace trigger trig 
-after insert on Transaction
+create or replace trigger trig
+before  insert on Transaction
 for each row
 declare
-    invalid EXCEPTION;
-	v_current NUMBER;
+	v_result NUMBER;
+	invalid EXCEPTION;
+cursor cur is
+    Select no_of_pieces from Stock;
+	cur_rec cur%ROWTYPE;
 begin
-    Select no_of_pieces into v_current from Stock where itemid=:NEW.itemid;
+	select no_of_pieces into v_result from Stock where itemid=:NEW.itemid;
 	if :NEW.type='Purchase' then
-		update Stock set no_of_pieces=v_current+:NEW.no_of_pieces where itemid=:NEW.itemid;
-		DBMS_OUTPUT.PUT_LINE('UPDATED');
+		UPDATE Stock set no_of_pieces=v_result+:NEW.no_of_pieces where itemid=:NEW.itemid;
 	elsif :NEW.type='Sales' then 
-        update Stock set no_of_pieces=v_current-:NEW.no_of_pieces where itemid=:NEW.itemid;
-		DBMS_OUTPUT.PUT_LINE('UPDATED');
+		UPDATE Stock set no_of_pieces=v_result-:NEW.no_of_pieces where itemid=:NEW.itemid;
 	else
         RAISE invalid;
 	end if;
+	open cur;
+	loop
+		fetch cur into cur_rec;
+		exit when cur%NOTFOUND;
+		DBMS_OUTPUT.PUT_LINE('Details:'||cur_rec.no_of_pieces);
+	end loop;
+close cur;
 EXCEPTION
-    when invalid then
-    	DBMS_OUTPUT.PUT_LINE('Invalid');
+	when invalid then
+		DBMS_OUTPUT.PUT_LINE('Invalid');
 end;
-
+		
+	
